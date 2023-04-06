bdee168b8637cf88f3e6ff58b0ae24013a7ce9f4
Omar Ebrahim <omarebrahim2014ggmail.com>
2023-04-07 00:07:21 +0200

    added sanity to the project as backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { StateContext } from '../context/StateContext';
+
+import { Layout } from '../components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
